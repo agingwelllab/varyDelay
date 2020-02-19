b39fe29a8a73bbfebb2d0be00882ab8476315d8b
Seaman <kls190004@cometnet-10-21-57-21.utdallas.edu>
2020-02-19 14:51:47 -0600

    Updates from Shelby

--- 06_health_ins_piechart.R
+++ 06_health_ins_piechart.R
@@ -1,5 +1,5 @@
 #pie chart of health insurance
-#SL 2.11.20
+#SL 2.19.20
 
 # load required packages
 library(here)
@@ -14,23 +14,20 @@ source(here::here('scr', 'isolate_data.R'))
 # load data
 dt <- read.csv(here::here('data', 'varydelay_data.csv'))
 
-#extract health insurance from data
+#data key (Qualtrics): 1= yes, 2= No, NA=NA; sample only has 1s
+
+#extract health_ins column from data
 health_ins <- dt$health_ins
 
-#make insurance categorical (factor)
-as.factor(health_ins)
+#make into health_ins frame- still has NA values
+hsFrame <- as.data.frame(health_ins)
 
-#get health_ins Frequencies & save as its own table
-health_ins_frequency <- count(health_ins)
-health_ins_frequency <- health_ins_frequency [c(1:3),]
-health_ins_frequency <- count(health_ins)
+#get frequency table of hsFrame
+hsFreq <- as.data.frame(table(hsFrame))
 
-#rename to health_ins & frequency
-names(health_ins_frequency) [1] = "Health_Ins"
-
-# create barchart of health_ins & frequency- getting stuck here!!!!!!
-ggplot(health_ins_frequency, aes(x="", y= freq, fill= health_ins)) + geom_bar (stat= "identity", width=1)
-
-#make it circular with coord_polar()
-ggplot(health_ins_frequency, aes(x="", y= freq, fill= health_ins)) + geom_bar (stat= "identity", width=1) +
-  coord_polar ("y", start = 0)
+#make hsFreq into piechart
+title <- "Do you currenly have health insurance?"
+x <-hsFreq$Freq
+#y <- hsFreq$hsFrame
+y <- c("yes")
+pie(x,y, main=title)
