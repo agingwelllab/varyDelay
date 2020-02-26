58bb7089ca7d7a6083ecb91c69ae8657c047b75f
Seaman <kls190004@cvltechs-iMac-3.local>
2020-02-26 13:38:28 -0600

    Cleaned up linear model script

--- 05_linear_model.R
+++ 05_linear_model.R
@@ -1,12 +1,10 @@
-# 1.29.20 KLS, CRG, SL
+# 2.26.20 KLS, CRG, SL
 # choice 1 = SS, choice 2 = LL
 
 # load required packages
 library(here)
 library(plyr)
-library(ggplot2)
 library(tidyverse)
-library(ez)
 
 # load source functions
 source(here::here('scr', 'isolate_data.R'))
@@ -23,45 +21,38 @@ gd <- gd[complete.cases(gd),]
 
 # create new variable/model - delay_n_days
 
-d1 <- gather(gd, "gambletype", "choice", X1d_1:X10y_005) 
+d1 <- gather(gd, "gambletype", "choice", X1d_1:X10y_005) # make gambletype column
 
-d1$delay <- as.factor(t(as.data.frame(strsplit(d1$gambletype, '_')))[,1])
-d1$kval <- as.factor(t(as.data.frame(strsplit(d1$gambletype, '_')))[,2])
+# break apart gamble type into delay and k val columns
+d1$delay <- as.factor(t(as.data.frame(strsplit(d1$gambletype, '_')))[,1]) #create delay column
+d1$kval <- as.factor(t(as.data.frame(strsplit(d1$gambletype, '_')))[,2]) # create k val column
 
+# convert the letter in delay variable to # of days (d for day = 1, w for week = 7, m for month = 30, 
+# y for year = 365)
 d1$delay_n_days <- ifelse(str_detect(d1$delay, 'd'), '1', 
                           ifelse(str_detect(d1$delay, 'w'), '7', 
                                  ifelse(str_detect(d1$delay, 'm'), '30',
                                         ifelse(str_detect(d1$delay, 'y'), '365', 0))))
 
-d1$delaym <- str_remove_all(d1$delay, "[Xdwmy]")
-d1$delayND <- as.numeric(d1$delay_n_days)
-d1$delayM <- as.numeric(d1$delaym)
-d1$choice <- as.numeric(d1$choice)
-d1$delay_n_days <- d1$delayM*d1$delayND
-d1 <- d1[, -which(names(d1) %in% c("delaym", "delayND", "delayM", "DelayDays", "gambletype", "delay"))]
-d1$delay_n_days <- str_replace(d1$delay_n_days, "28", "30") 
-d1$delay_n_days <-str_replace(d1$delay_n_days, "360", "365")
-d1$choice <- str_replace(d1$choice, "1", "0")
-d1$choice <- str_replace(d1$choice, "2", "1")
-d1$choice <- as.numeric(d1$choice)
+# isolate the number from delay variable 
+d1$delay <- str_remove_all(d1$delay, "[Xdwmy]")
+#d1$gambletype <- NULL # remove gamble type variable
+
+d1$delay <- as.numeric(d1$delay)
 d1$delay_n_days <- as.numeric(d1$delay_n_days)
+d1$delay_n_days <- d1$delay*d1$delay_n_days
+d1$delay_n_days <- as.numeric(str_replace(d1$delay_n_days, "28", "30")) 
+d1$delay_n_days <- as.numeric(str_replace(d1$delay_n_days, "360", "365"))
+
+# convert k vals to numeric
+d1$kval <- paste0('.', d1$kval) # more concise
+d1$kval <- as.numeric(as.character(d1$kval))
+
+# recode choice into accept (1) or reject (0)
+d1$choice <- d1$choice - 1 # more concise
+d1$choice <- as.numeric(d1$choice)
 
 # Simple Logistic Regression
 
 M2 <- glm(d1$choice~d1$Age+d1$kval+d1$delay_n_days,family=binomial(link='logit'), data = d1)
-
-# 7dvs1w Model
-
-d2 <- gd[,c(1, 2, 9:14, 18:23, 27:32)]
-d2 <- gather(d2, "gambletype", "choice", X7d_1:X1y_1) 
-
-d2$delay <- as.factor(t(as.data.frame(strsplit(d2$gambletype, '_')))[,1])
-d2$kval <- as.factor(t(as.data.frame(strsplit(d2$gambletype, '_')))[,2])
-
-d2$delay_unit <- ifelse(str_detect(d2$delay, 'd'), 'days', 
-                        ifelse(str_detect(d2$delay, 'w'), 'weeks', 
-                               ifelse(str_detect(d2$delay, 'm'), 'months',
-                                      ifelse(str_detect(d2$delay, 'y'), 'years', 0))))
-d2$delay <- str_remove_all(d2$delay, "[Xdwmy]")
-d2 <- as.data.frame(t(d2))
 
