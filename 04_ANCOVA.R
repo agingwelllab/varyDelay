caf94c8b20cb3f60975276d6fdee6ad9db92ef9a
Seaman <kls190004@cvltechs-imac-3.lan>
2021-07-29 18:29:12 -0500

    update anova and linear scripts

--- 04_ANCOVA.R
+++ 04_ANCOVA.R
@@ -9,6 +9,7 @@ library(ez)
 library(sjPlot)
 library(knitr)
 library(lme4)
+library(psych)
 
 # load source functions
 source(here::here('scr', 'isolate_data.R'))
@@ -26,7 +27,7 @@ grep('X1d_1', colnames(dt)):grep('X10y_005', colnames(dt))))
 
 gd <- gd[complete.cases(gd),]
 
-# restructure data for analysis 
+# restructure data for analysis ####
 d0 <- gather(gd, "gambletype", "choice", X1d_1:X10y_005)
 rm(gd)
 
@@ -37,13 +38,14 @@ d0$delay_unit <- ifelse(str_detect(d0$delay, 'd'), 'days',
        ifelse(str_detect(d0$delay, 'w'), 'weeks', 
               ifelse(str_detect(d0$delay, 'm'), 'months',
                      ifelse(str_detect(d0$delay, 'y'), 'years', 0))))
+d0$delay_unit <- factor(d0$delay_unit, levels = c("days", "weeks", "months", "years"))
 
 ## Center age
 d0$Age <- scale(d0$Age, center = TRUE, scale=TRUE)
 
 # choice 1 = SS, choice 2 = LL
 
-# 4 (delay_unit) x Age Within-Subjects ANCOVA
+# 4 (delay_unit) x Age Within-Subjects ANCOVA ####
 m1 <- ezANOVA(data = d0, dv = .(choice), wid = .(ID), within = .(delay_unit), between = .(Age), return_aov = TRUE, detailed = TRUE)
 saveRDS(m1, here::here('output', 'model1.RDS'))
 
@@ -56,51 +58,55 @@ d0$choice <- as.numeric(d0$choice)
 
 d0$agegrp <- ifelse(d0$Age > median(d0$Age), 'Older', 'Younger')
 
-# get means for each delay unit
-d1 <- summarySE(data=d0, measurevar="choice", groupvars=c("ID", "agegrp","delay_unit"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
+# Follow-up tests ####
 
-d1 <- d1 %>% pivot_wider(id_cols = c('ID', 'agegrp'),  names_from = delay_unit, values_from = choice)
-d2 <- d1[which(d1$agegrp == 'Younger'), ]
-d3 <- d1[which(d1$agegrp == 'Older'), ]
-
-younger <- pairedttable(d2[c(1,3:6)], colnames(d2[3:6]))
-younger$tval
-younger$`p values`
-saveRDS(younger, here::here('output', 'youngerttest.RDS'))
-
-older <- pairedttable(d3[c(1,3:6)], colnames(d3[3:6]))
-older$tval
-older$`p values`           
-saveRDS(older, here::here('output', 'olderttest.RDS'))
+# main effect of delay unit ####
+d1 <- summarySE(data=d0, measurevar="choice", groupvars=c("ID", 'Age', "agegrp", "delay_unit"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
+d1 <- d1 %>% pivot_wider(id_cols = c('ID', 'Age', 'agegrp'),  names_from = delay_unit, values_from = choice)
 
 #overall means and comparison
-d6 <- colMeans(d1[3:6])
-all <- pairedttable(d1[c(1,3:6)], colnames(d1[3:6]))
-
-# between-subs t-tests
-days_t <- t.test(d2$days, d3$days)
-weeks_t <- t.test(d2$weeks, d3$weeks)
-months_t <- t.test(d2$months, d3$months)
-years_t <- t.test(d2$years, d3$years)
+d6 <- colMeans(d1[4:7])
+all <- pairedttable(d1[c(1,4:7)], colnames(d1[3:6]))
 
 # Reorder data for tables
-d1 <- summarySE(data=d0, measurevar="choice", groupvars=c("delay_unit"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
+d2 <- summarySE(data=d0, measurevar="choice", groupvars=c("delay_unit"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
 
 # sjPlot table for Fig. 1 (Saved as CSV)
 
-tab_df(d1[, c(1, 3:4)],
+tab_df(d2[, c(1, 3:4)],
         col.header = c("Delay Unit", "Choice", "Std. Deviation"),
         title = "Mean and standard deviation of proportion of smaller, sooner options selected by delay length")       
 
 # Post-hoc test for M1
 PHM1 <- pairwise.t.test(d0$choice, d0$delay_unit, paired = T, p.adjust.method = "holm")
 M1PHT <-as.table(PHM1$p.value)
-
+M1PHT <-t(round(M1PHT, 3))
+ 
 knitr::kable(list(M1PHT))
 write.csv(M1PHT, here::here("figs", "Post_Hoc_Test_M1.csv"))
 dev.off()
 
-# Mixed effect model
+# interaction between age and delay_unit 
+days <- cor.test(d1$Age, d1$days)
+weeks <- cor.test(d1$Age, d1$weeks)
+months <- cor.test(d1$Age, d1$months)
+years <- cor.test(d1$Age, d1$years)
+
+add_sig <- function(x) {
+  r <- round(x$estimate, 3) 
+  p <- x$p.value
+  sig05 <- p < .05 & (!is.na(p)); sig01 <- p < .01  & (!is.na(p)); sig001 <- p < .001  & (!is.na(p))
+  if (sig05 == TRUE) {r <- paste0(as.character(r), "*")}
+  if (sig01 == TRUE) {r <- paste0(as.character(r), "*")}
+  if (sig001 == TRUE) {r <- paste0(as.character(r), "*")}
+  return(r)
+}
+
+cAge = c(add_sig(days), add_sig(weeks), add_sig(months), add_sig(years))
+
+write.csv(cAge, here::here('figs', 'Post_Hoc_Test_Interaction_M1.csv'))
+
+# Mixed effect model ####
 
 MM1 <- lmer(choice~Age + (1| delay_unit) +(1| kval), data = d0)
 summary(MM1)
--- 05_linear_model.R
+++ 05_linear_model.R
@@ -10,6 +10,7 @@ library(reghelper)
 library(broom)
 library(sjPlot)
 library(rmcorr)
+
 # load source functions
 source(here::here('scr', 'isolate_data.R'))
 source(here::here('scr', 'summarySE.R'))
@@ -20,7 +21,7 @@ source(here::here('scr', 'logistic_pseudoR2.R'))
 # load data
 dt <- read.csv(here::here('data', 'varydelay_data.csv'))
 
-# isolate gamble data
+# isolate gamble data ####
 gd <- isolate_data(dt, grep('ID', colnames(dt))[1], c(grep('Age', colnames(dt)), 
                                                       grep('X1d_1', colnames(dt)):grep('X10y_005', colnames(dt))))
 gd <- gd[complete.cases(gd),]
@@ -70,18 +71,55 @@ hist(d1$sqrtdd)
 d1$logdnd <- log(d1$delay_n_days)
 hist(d1$logdnd)
 #logdnd wins!
+
 # Create factor version of logdnd (For use in correlation matrix)
 d1$logdndl <- factor(d1$logdnd,
                      levels = c(0, 1.38629436111989, 1.94591014905531, 2.63905732961526, 3.40119738166216, 5.19295685089021, 5.89989735358249, 7.50933526601659, 8.20248244657654),
                      labels = c("1", "4", "7", "14", "30", "180", "365", "1825", "3650"))
 
-# Simple Logistic Regression
+
+# Simple Logistic Regression ####
 #M2 <- glm(d1$choice ~ d1$Age * d1$delay_n_days, family = binomial(link = 'logit'), data = d1)
 #summary(M2)
 
 M2 <- glm(choice ~ Age * logdnd, family = binomial(link = 'logit'), data = d1)
 SM2 <- summary.glm(M2, correlation = TRUE, signif.stars = TRUE) 
 print(SM2)
+
+#find standardzied coefficients (beta)
+stdz.coff <- function (regmodel)
+{ b <- summary(regmodel)$coef[-1,1]
+sx <- sapply(regmodel$model[-1], sd)
+beta <-(3^(1/2))/pi * sx * b
+return(beta)
+}
+stdz.coff(M2)
+
+# Means(SDs) and follow-up tests
+d2 <- d1 %>% dplyr::group_by(delay_n_days) %>% dplyr::summarize(
+  mean = round(mean(choice), 2),  
+  sd = round(sd(choice), 2), 
+  estimate = cor.test(Age, choice)$estimate, 
+  pvalue = cor.test(Age, choice)$p.value 
+)
+
+
+d2 <- d2 %>% mutate(
+  estimate = round(estimate,3),
+  sig05 = pvalue < .05 & (!is.na(pvalue)),
+  sig01 = pvalue < .01  & (!is.na(pvalue)),
+  sig001 = pvalue < .001  & (!is.na(pvalue)),
+  stars = ifelse(sig001 == TRUE, "***", 
+         ifelse(sig01 == TRUE, "**", 
+         ifelse(sig05 == TRUE, "*", ""))), 
+  combined_est = paste0(as.character(estimate), stars)
+)
+
+d2 <- d2[c(1:3, 10)]
+
+write.csv(d2, here::here('figs', 'means_sd_corr_M2.csv'), row.names = FALSE)
+
+# Assumptions Testing ####
 
 # Test for linearity
 # Collect variables and graph for linear logit relationship
@@ -117,7 +155,7 @@ ggplot(model.data, aes(index, .std.resid)) +
   geom_point(aes(color = d1$delay_n_days), alpha = .5) +
   theme_bw()
 
-
+# Additional regression models ####
 # Discount rate added regression model
 #M3 <- glm(d1$choice ~ d1$Age * d1$delay_n_days * d1$kval, family = binomial(link = 'logit'), data = d1)
 #summary(M3)
@@ -125,34 +163,24 @@ ggplot(model.data, aes(index, .std.resid)) +
 M3 <- glm(choice ~ Age * logdnd * kval, family = binomial(link = 'logit'), data = d1)
 summary(M3)
 
-#find standardzied coefficients (beta)- just kidding.
-#standardized_betas <- beta(M2)
 
 
-stdz.coff <- function (regmodel)
-{ b <- summary(regmodel)$coef[-1,1]
-sx <- sapply(regmodel$model[-1], sd)
-beta <-(3^(1/2))/pi * sx * b
-return(beta)
-}
-stdz.coff(M2)
 
-
-#Build Correlation Matrix Table
-
-M2C <- glm(choice ~ Age * logdndl, family = binomial(link = 'logit'), data = d1)
-SM2C <- summary.glm(M2C, 
-                    correlation = TRUE) 
-print(SM2C$correlation[c(2, 11:18), c(2, 11:18)])
-
-# Table of correlations between proportion of smaller, sooner options chosne and age, by length of delay in log days (Table 3)
-
-tab_corr(SM2C$correlation[c(2, 11:18), c(2, 11:18)], 
-         triangle = "l",
-         show.p = TRUE, p.numeric = TRUE,
-         title = "Correlation of age and proportion of smaller sooner options, by log of delay in days",
-         var.labels = c("Log 1", " Log 4", "Log 7", "Log 14", "Log 30", "Log 180", "Log 365", "Log 1825", "Log 3650")
-         )
+# #Build Correlation Matrix Table - I don't think this code does what you think it does....
+# 
+# M2C <- glm(choice ~ Age * logdndl, family = binomial(link = 'logit'), data = d1)
+# SM2C <- summary.glm(M2C, 
+#                     correlation = TRUE) 
+# print(SM2C$correlation[c(2, 11:18), c(2, 11:18)])
+# 
+# # Table of correlations between proportion of smaller, sooner options chosne and age, by length of delay in log days (Table 3)
+# 
+# tab_corr(SM2C$correlation[c(2, 11:18), c(2, 11:18)], 
+#          triangle = "l",
+#          show.p = TRUE, p.numeric = TRUE,
+#          title = "Correlation of age and proportion of smaller sooner options, by log of delay in days",
+#          var.labels = c("Log 1", " Log 4", "Log 7", "Log 14", "Log 30", "Log 180", "Log 365", "Log 1825", "Log 3650")
+#          )
 
 # sjPlot Tables (Table 4)
 
