301b0da878476ec662f303c30e111b6a211a0c7c
Seaman <kls190004@cvltechs-imac-3.lan>
2021-07-29 14:06:37 -0500

    update Figs 1, S1, S2

--- 07_graph_by_delay_unit.R
+++ 07_graph_by_delay_unit.R
@@ -24,7 +24,7 @@ gd <- isolate_data(dt, grep('ID', colnames(dt))[1], c(grep('Age', colnames(dt)),
 
 gd <- gd[complete.cases(gd),]
 
-# restructure data for analysis 
+# restructure data for graphing ####
 d0 <- gather(gd, "gambletype", "choice", X1d_1:X10y_005)
 rm(gd)
 
@@ -43,7 +43,21 @@ d0$choice <- as.numeric(d0$choice)
 # Add Age group
 d0$agegrp <- ifelse(d0$Age > median(d0$Age), 'Older', 'Younger')
 
-# graph of averages across delay unit and kvalue (Now breaks down by age group also.)
+# Figure 1 ####
+fig1 <- ggplot(d0, aes(Age, choice, fill = delay_unit, colour = delay_unit)) + 
+  geom_smooth(method = lm) + theme_bw() + ylab('Proportion SS Choice') + xlab('Age') + 
+  scale_fill_discrete(name="Delay Condition") + scale_colour_discrete(name="Delay Condition") + 
+  theme(plot.title = element_text(face="bold", size = 24),axis.title.x = element_text(size = 24), 
+        axis.title.y = element_text(size = 24), axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), 
+        strip.text.x = element_text(size=20), legend.text = element_text(size = 16), legend.title = element_text(size = 20), 
+        legend.position = 'top')
+
+png(here::here('figs', 'propSS_x_age_x_delay_unit.png'), width = 600, height = 600)
+fig1
+dev.off()
+
+
+# Figure S1 - graph of averages across delay unit and kvalue (Now breaks down by age group also) ####
 d1 <- summarySE(data=d0, measurevar="choice", groupvars=c("agegrp", "delay_unit","kval"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
 
 d1$delay_unit <- factor(d1$delay_unit, levels = c("days", "weeks", "months", "years"))
@@ -57,53 +71,8 @@ graph_delay_unit_kval <- ggplot(d1, aes(delay_unit, choice, fill = kval)) +
 
 graph_delay_unit_kval
 
-# graph of averages across delay unit and kvalue
-d2 <- summarySE(data=d0, measurevar="choice", groupvars=c("agegrp","delay_unit","kval"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
-d2$delay_unit <- factor(d2$delay_unit, levels = c("days", "weeks", "months", "years"))
 
-graph_delay_unit_kval_age <- ggplot(d2, aes(kval, choice, fill = kval)) + 
-  geom_bar(stat='identity', position=position_dodge()) +
-  geom_errorbar(aes(ymin=choice-se, ymax=choice+se), width=.2, position=position_dodge(.9)) + 
-  theme_minimal() + ylab('Proportion SS Choice') + xlab('kval') + facet_grid(delay_unit~agegrp) + theme(legend.position = 'null')
-
-# graph of averages across delay unit by age - FIGURE 1
-d3 <- summarySE(data=d0, measurevar="choice", groupvars=c("agegrp","delay_unit"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
-d3$delay_unit <- ordered(d3$delay_unit, levels = c("days", "weeks", "months", "years"))
-
-graph_delay_unit_age <- ggplot(d3, aes(delay_unit, choice, fill = agegrp)) + 
-  geom_bar(stat= 'identity', position=position_dodge()) + 
-  geom_errorbar(aes(ymin=choice-se, ymax=choice+se), width=.2, position=position_dodge(.9)) +
-  theme_minimal() + theme(plot.title = element_text(face="bold", size = 24),
-                            axis.title.x = element_text(size = 24), axis.title.y = element_text(size = 24),
-                            axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20),
-                            strip.text.x = element_text(size=20), 
-                          legend.text = element_text(size = 16), legend.title = element_text(size = 20), legend.position = 'top') + 
-  scale_fill_discrete(name = "Age Group") + scale_color_discrete(name = "Age Group") + 
-  ylab('Proportion SS Choice') + xlab('Delay Unit') 
-  
-graph_delay_unit_age
-
-# Raincloud graphs
-d2$delay_unit <- ordered(d2$delay_unit, levels = c("days", "weeks", "months", "years"))
-ggplot(d2, aes(x = delay_unit, y = choice, fill = agegrp)) + 
-  ggdist::stat_halfeye(
-    adjust = .5,
-    width = .6, 
-    ## set slab interval to show IQR and 95% data range
-    .width = c(.5, .95)
-  )  +
-  coord_cartesian(xlim = c(1.2, NA)) +
-  facet_grid(rows = vars(agegrp)) +
-  coord_flip() +
-  theme_minimal() + theme(plot.title = element_text(face="bold", size = 24),
-                          axis.title.x = element_text(size = 24), axis.title.y = element_text(size = 24),
-                          axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20),
-                          strip.text.x = element_text(size=20), 
-                          legend.text = element_text(size = 16), legend.title = element_text(size = 20), legend.position = 'top') + 
-  scale_fill_hue(name = "Age Group") + scale_color_grey(name = "Age Group") + 
-  ylab('Proportion of Smaller Sooner Choices') + xlab('Delay Unit') 
-
-# Stacked Graph
+# Figure S2 - Stacked Graph of distributions ####
 ggplot(d2, aes(x = delay_unit, y = choice, fill = agegrp, alpha = 0.5)) + 
   ggdist::stat_halfeye(
     adjust = .5,
@@ -121,7 +90,11 @@ ggplot(d2, aes(x = delay_unit, y = choice, fill = agegrp, alpha = 0.5)) +
   scale_fill_hue(name = "Age Group") + scale_color_grey(name = "Age Group") + 
   ylab('Proportion of Smaller Sooner Choices') + xlab('Delay Unit') 
 
-# main effect of delay unit
+png(here::here('figs', 'stacked_distribution_propSS_x_age_grp.png'), width = 600, height = 600)
+stacked
+dev.off()
+
+# main effect of delay unit ####
 
 d4 <- summarySE(data=d0, measurevar="choice", groupvars=c("delay_unit"), na.rm=FALSE, conf.interval=.95, .drop=TRUE)
 
@@ -137,15 +110,6 @@ graph_delay_unit <- ggplot(d4, aes(delay_unit, choice, fill = delay_unit)) +
 
 graph_delay_unit
 
-#save graphs
-png(here::here('figs', 'delay_unit_x_age_grp.png'), width = 600, height = 600)
-graph_delay_unit_age
-dev.off()
-
-png(here::here('figs', 'stacked_distribution_propSS_x_age_grp.png'), width = 600, height = 600)
-stacked
-dev.off()
-
 png(here::here('figs', 'main_effect_of_delay_unit.png'), width = 600, height = 600)
-graph_delay_unit_age
+graph_delay_unit
 dev.off()
